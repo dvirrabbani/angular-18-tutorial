dd71840e4b35e4fe12aac9b5a8ae2631c03ab9a6
Dvir <rabbanidvir@gmail.com>
2024-10-24 19:20:47 +0300

    link to home page via brand logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [HomeComponent, RouterLink, RouterOutlet],
   styleUrl: './app.component.scss',
   template: `
     <main>
+      <a [routerLink]="['/']">
       <header class="brand-name">
         <!-- decorative image -->
         <img src="/assets/logo.svg" alt="logo" aria-hidden="true">
       </header>
+      </a>
       <section class="content">
            <router-outlet></router-outlet>
       </section>
